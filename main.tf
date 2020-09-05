70d7217c44b3ec5ec6858a45980d6d6a82cb6162
Vlad <vlad@ghn.me>
2020-09-05 14:23:55 -0500

    Remove VPC

--- main.tf
+++ main.tf
@@ -55,36 +55,36 @@ module "cloudtrail" {
   common_tags = var.common_tags
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 2.0"
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "~> 2.0"
 
-  name = "VGH"
-  cidr = "10.0.0.0/16"
+#   name = "VGH"
+#   cidr = "10.0.0.0/16"
 
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
 
-  azs = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c",
-    "us-east-1d",
-    "us-east-1e",
-    "us-east-1f",
-  ]
+#   azs = [
+#     "us-east-1a",
+#     "us-east-1b",
+#     "us-east-1c",
+#     "us-east-1d",
+#     "us-east-1e",
+#     "us-east-1f",
+#   ]
 
-  public_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/24",
-  ]
+#   public_subnets = [
+#     "10.0.1.0/24",
+#     "10.0.2.0/24",
+#     "10.0.3.0/24",
+#     "10.0.4.0/24",
+#     "10.0.5.0/24",
+#     "10.0.6.0/24",
+#   ]
 
-  tags = var.common_tags
-}
+#   tags = var.common_tags
+# }
 
 # module "vault" {
 #   source = "github.com/vghn/terraform-vault"
